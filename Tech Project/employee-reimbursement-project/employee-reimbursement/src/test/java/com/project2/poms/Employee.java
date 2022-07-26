771855b00a8c1688bdae0b29e44562bacf09a127
Your Name <you@example.com>
2022-07-26 16:33:09 -0400

    pom files

---
+++ Tech Project/employee-reimbursement-project/employee-reimbursement/src/test/java/com/project2/poms/Employee.java
@@ -0,0 +1,60 @@
+package com.project2.poms;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Employee {
+    
+    @FindBy(className ="formgroup")
+    public WebElement EmployeeId;
+    @FindBy(className = "formgroup1")
+    public WebElement RequestAmount;
+    @FindBy(className ="formgroup2")
+    public WebElement RequestDescription;
+        @FindBy(className ="btnprimary")
+    public WebElement Submit;
+    @FindBy(className ="btnprimary1")
+    public WebElement LogOut;
+
+
+    public Employee(WebDriver driver){
+        PageFactory.initElements(driver, this);
+
+    } 
+    
+    public void employeeIdInput(String formGroup){
+    
+        this.EmployeeId.sendKeys(formGroup);
+    
+    }
+
+    public void employeeRequest(String formGroup1){
+    
+        this.RequestAmount.sendKeys(formGroup1);
+    
+    }
+
+    public void employeeDescription(String formGroup2){
+    
+        this.RequestDescription.sendKeys(formGroup2);
+    
+    }
+
+    public void clickButton(){
+
+        this.Submit.click();
+    }
+
+    public void clickButton1(){
+
+        this.LogOut.click();
+    
+    }
+
+
+
+
+
+}
---
+++ Tech Project/employee-reimbursement-project/employee-reimbursement/src/test/java/com/project2/steps/EmployeeSteps.java
@@ -0,0 +1,71 @@
+package com.project2.steps;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class EmployeeSteps {
+
+    /*
+        Scenario for employee logging in 
+        check the status on my reimbursement request
+    */
+
+    @When("The employee checks on current status")
+    public void the_employee_checks_on_current_status() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The employee should see more details")
+    public void the_employee_should_see_more_details() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    /*
+        As an employee I want to create and submit a new 
+        reimbursement request
+    */
+
+
+    @When("The employee enters a dollar amount less than or equal to $1000")
+    public void the_employee_enters_a_dollar_amount_less_than_or_equal_to_$1000() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("The employee selects from pre-approved list")
+    public void the_employee_selects_from_pre_approved_list() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("The employee clicks on the submit button")
+    public void the_employee_clicks_on_the_submit_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The employee sees a success notification")
+    public void the_employee_sees_a_success_notification() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    } 
+
+    /*
+        As an employee I need to close the session by logging out 
+    */
+    
+    @When("The employee clicks the button to sign out")
+    public void the_employee_clicks_the_button_to_sign_out() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The employee should be logged out and be redirected to the homepage")
+    public void the_employee_should_be_logged_out_and_be_redirected_to_the_homepage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
