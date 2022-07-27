47de71fd4dc67cf133ac8cba507bd170e8720bfc
Your Name <you@example.com>
2022-07-27 11:54:08 -0400

    login steps and manager steps fin

--- Tech Project/employee-reimbursement-project/employee-reimbursement/src/test/java/com/project2/runner/TestRunner.java
+++ Tech Project/employee-reimbursement-project/employee-reimbursement/src/test/java/com/project2/runner/TestRunner.java
@@ -1,6 +1,17 @@
 package com.project2.runner;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.project2.poms.Employee;
+import com.project2.poms.Login;
+import com.project2.poms.Manager;
+import com.project2.repository.ReimbursementDAOInterface;
+import com.project2.repository.ReimbursementDao;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
@@ -9,5 +20,30 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(features = "classpath:features", glue = "com/project2/steps", plugin = "pretty")
 public class TestRunner {
 
+        public static WebDriver driver;        
+        public static WebDriverWait wait;
+        public static Manager manager;
+        public static Employee employee;
+        public static Login login;
+
+    @BeforeClass
+    public static void setup(){
+
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/features/chromedriver.exe");
+        driver = new ChromeDriver();
+        
+        ReimbursementDAOInterface reimbursementDao = new ReimbursementDao();
+
+        driver= new ChromeDriver(); 
+        login= new Login(driver);
+        wait = new WebDriverWait(driver,7);
+        employee = new Employee(driver);
+        manager = new Manager(driver);
+}
+@AfterClass public static void Teardown(){
+    driver.quit();
+}
+
+    
     
 }
--- Tech Project/employee-reimbursement-project/employee-reimbursement/src/test/java/com/project2/steps/ManagerSteps.java
+++ Tech Project/employee-reimbursement-project/employee-reimbursement/src/test/java/com/project2/steps/ManagerSteps.java
@@ -1,43 +1,17 @@
 package com.project2.steps;
 
+import org.junit.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import com.project2.runner.TestRunner;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class ManagerSteps {
 
-    /*
-        As the manager, I want to view tables in the database 
-    */
-    @Given("the employee is on the login page")
-    public void the_employee_is_on_the_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    
-    @When("the manager enters his correct username")
-    public void the_manager_enters_his_correct_username() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("the manager enters his correct password")
-    public void the_manager_enters_his_correct_password() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("the employee clicks the login button")
-    public void the_employee_clicks_the_login_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("the manager should be logged in to the manager home page")
-    public void the_manager_should_be_logged_in_to_the_manager_home_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+       
     /*
         As a manager, I want to approve reimbursement requests
 
@@ -45,25 +19,27 @@ public class ManagerSteps {
     @Given("The manager is logged into the database")
     public void the_manager_is_logged_into_the_database() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        TestRunner.driver.get("file:///C:/Users/17132/Desktop/P2-Employee-Reimbursement/Tech%20Project/employee-reimbursement-project/employee-reimbursement/src/main/resources/web%20pages/manager.html");
     }
 
     @When("The manager views reimbursement requests")
     public void the_manager_views_reimbursement_requests() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        TestRunner.manager.requestInput("");
     }
 
     @When("The manager selects list to approve request")
     public void the_manager_selects_list_to_approve_request() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        TestRunner.manager.clickButton();
     }
 
     @Then("The manager should be able to save all changes")
     public void the_manager_should_be_able_to_save_all_changes() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        TestRunner.wait.until(ExpectedConditions.alertIsPresent());
+                    String text = TestRunner.driver.switchTo().alert().getText();
+                    Assert.assertEquals("Request Approved", text);
     }
         /*
             The manager should be able to deny reimbursement requests 
@@ -71,19 +47,21 @@ public class ManagerSteps {
     @When("The manager views reimbursement request")
     public void the_manager_views_reimbursement_request() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        TestRunner.manager.requestInput("");
     }
 
     @When("The manager selects list to deny request")
     public void the_manager_selects_list_to_deny_request() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        TestRunner.manager.clickButton1();
     }
 
     @Then("The manager should be able to save  changes")
     public void the_manager_should_be_able_to_save_changes() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        TestRunner.wait.until(ExpectedConditions.alertIsPresent());
+                    String text = TestRunner.driver.switchTo().alert().getText();
+                    Assert.assertEquals("Request Denied", text);
     }
 
         /*
@@ -92,13 +70,13 @@ public class ManagerSteps {
     @When("The manager clicks the button to sign out")
     public void the_manager_clicks_the_button_to_sign_out() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        TestRunner.manager.clickButton2();
     }
 
     @Then("The manager should log out and be redirected to the homepage")
     public void the_manager_should_log_out_and_be_redirected_to_the_homepage() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        TestRunner.driver.get("file:///C:/Users/17132/Desktop/P2-Employee-Reimbursement/Tech%20Project/employee-reimbursement-project/employee-reimbursement/src/main/resources/web%20pages/employee.html");
     }
 
 }
