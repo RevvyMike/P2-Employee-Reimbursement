c03d00ebd5b6a743276f07f03b19219c0113331f
RevvyMike <Michael619@revature.net>
2022-07-30 19:26:30 -0500

    Updated (6 Errors) left to fix

--- Tech Project/employee-reimbursement-project/employee-reimbursement/src/test/java/com/project2/steps/EmployeeSteps.java
+++ Tech Project/employee-reimbursement-project/employee-reimbursement/src/test/java/com/project2/steps/EmployeeSteps.java
@@ -1,82 +1,98 @@
 package com.project2.steps;
+
 import org.junit.Assert;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import com.project2.runner.TestRunner;
 
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class EmployeeSteps {
+    
 
-    /*
-        Scenario for employee logging in 
-        check the status on my reimbursement request
-    */
+    @Given("The employee is on the on their homepage")
+    public void the_employee_is_on_the_on_their_homepage() {
+        TestRunner.driver.get("File://C:/Users/Mike/Desktop/P2-Employee-Reimbursement/Tech Project/employee-reimbursement-project/employee-reimbursement/src/main/resources/web pages/EMPLOYEEdavid.html");
+}
 
-    @When("The employee checks on current status")
-    public void the_employee_checks_on_current_status() {
-        // Write code here that turns the phrase above into concrete actions
-        TestRunner.driver.get("C:/Users/17132/Desktop/P2-Employee-Reimbursement/Tech Project/employee-reimbursement-project/employee-reimbursement/src/main/resources/web pages/EMPLOYEEdavid.html");
+
+    @When("The employee views the remibursement requests")
+    public void the_employee_views_the_remibursement_requests() {
+        TestRunner.driver.get("File://C:/Users/Mike/Desktop/P2-Employee-Reimbursement/Tech Project/employee-reimbursement-project/employee-reimbursement/src/main/resources/web pages/EMPLOYEEdavid.html");
     }
 
-     /*
-        As an employee I want to create and submit a new 
-        reimbursement request
-    */
+    @Then("The employee should be ready to create new request")
+    public void the_employee_should_be_ready_to_create_new_request() {
+        TestRunner.driver.get("File://C:/Users/Mike/Desktop/P2-Employee-Reimbursement/Tech Project/employee-reimbursement-project/employee-reimbursement/src/main/resources/web pages/EMPLOYEEdavid.html");}
 
-    @When("The employee enters the employees username")
-    public void the_employee_enters_the_employees_username() {
-        // Write code here that turns the phrase above into concrete actions
-        TestRunner.employee.employeeIdInput("Leonardo");
-    }
 
-   
-    @When("The employee enters a dollar amount less than or equal to $1000")
-    public void the_employee_enters_a_dollar_amount_less_than_or_equal_to_$1000() {
-        // Write code here that turns the phrase above into concrete actions
+    @Given("The employee is in request field on homepage")
+    public void the_employee_is_in_request_field_on_homepage() {
+        TestRunner.driver.get("File://C:/Users/Mike/Desktop/P2-Employee-Reimbursement/Tech Project/employee-reimbursement-project/employee-reimbursement/src/main/resources/web pages/EMPLOYEEdavid.html");
+}
+
+
+    @When("The employee types request reason into request reason input")
+    public void the_employee_types_request_reason_into_request_reason_input() {
+        TestRunner.employee.employeeDescription("Fixing a manhole downtown");
+}
+
+
+    @When("The employee types dollar amount into dollar request input")
+    public void the_employee_types_dollar_amount_into_dollar_request_input() {
         TestRunner.employee.employeeRequest(100);
     }
+    @When("Then employee clicks submit request button")
+    public void then_employee_clicks_submit_request_button() {
+      TestRunner.employee.clickButton();
+}
 
-    @When("The employee enters a request description")
-    public void the_employee_enters_a_request_description() {
-        // Write code here that turns the phrase above into concrete actions
-        TestRunner.employee.employeeDescription("");
-    }
 
-    @When("The employee clicks on the submit button")
-    public void the_employee_clicks_on_the_submit_button() {
-        // Write code here that turns the phrase above into concrete actions
-        TestRunner.employee.clickButton();
-    }
+    @Then("The employee should be ready to log out")
+    public void the_employee_should_be_ready_to_log_out() {
+    TestRunner.wait.until(ExpectedConditions.alertIsPresent());
+    String text = TestRunner.driver.switchTo().alert().getText();
+    Assert.assertEquals("you are logged out", text);
+    TestRunner.driver.switchTo().alert().accept();
+    TestRunner.wait.until(ExpectedConditions.titleIs("AAA Login Page"));
+    String title = TestRunner.driver.getTitle();
+    Assert.assertEquals("AAA Login Page", title);
+}
 
-    @Then("The employee sees a success notification")
-    public void the_employee_sees_a_success_notification() {
-        // Write code here that turns the phrase above into concrete actions
+
+
+
+
+
+
+    @Given("The employee is on the manager homepage")
+public void the_employee_is_on_the_manager_homepage() {
+    TestRunner.driver.get("File://C:/Users/Mike/Desktop/P2-Employee-Reimbursement/Tech Project/employee-reimbursement-project/employee-reimbursement/src/main/resources/web pages/EMPLOYEEdavid.html");
+}
+
+    @When("The employee clicks log out button")
+    public void the_employee_clicks_log_out_button() {
+        TestRunner.employee.clickButton1();
+}
+    @Then("The employee should be logged out")
+    public void the_employee_should_be_logged_out() {
         TestRunner.wait.until(ExpectedConditions.alertIsPresent());
         String text = TestRunner.driver.switchTo().alert().getText();
-        Assert.assertEquals("Request Submitted", text);
+        Assert.assertEquals("you are logged out", text);
         TestRunner.driver.switchTo().alert().accept();
-    } 
+        TestRunner.wait.until(ExpectedConditions.titleIs("AAA Login Page"));
+        String title = TestRunner.driver.getTitle();
+        Assert.assertEquals("AAA Login Page", title);
 
-    /*
-        As an employee I need to close the session by logging out 
-    */
-    
-    @When("The employee clicks the button to sign out")
-    public void the_employee_clicks_the_button_to_sign_out() {
-        // Write code here that turns the phrase above into concrete actions
-        TestRunner.employee.clickButton1();
-    }
+}
 
-    @Then("The employee should be logged out and be redirected to the homepage")
-    public void the_employee_should_be_logged_out_and_be_redirected_to_the_homepage() {
-        // Write code here that turns the phrase above into concrete actions
-        String text = TestRunner.driver.switchTo().alert().getText();
-        Assert.assertEquals("You are logged out", text);
-        TestRunner.driver.switchTo().alert().accept();
-        TestRunner.driver.get("file:///C:/Users/17132/Desktop/P2-Employee-Reimbursement/Tech%20Project/employee-reimbursement-project/employee-reimbursement/src/main/resources/web%20pages/login.html");
-    }
 
 
 }
+
+
+
+
+  
--- Tech Project/employee-reimbursement-project/employee-reimbursement/src/test/resources/features/Employee.feature
+++ Tech Project/employee-reimbursement-project/employee-reimbursement/src/test/resources/features/Employee.feature
@@ -3,12 +3,12 @@ Feature: Employee should be able to view previous reimbursement requests
     Scenario: As an employee I want to view a reimbursement request so I see if I can get money back
 
         Given   The employee is on the on their homepage
-        When    The employee views the remibursement requests
-        Then    The employee should be ready to create new request
+        # When    The employee views the remibursement requests
+        # Then    The employee should be ready to create new request
 
     Scenario: As an employee I want create a reimbursement request so I can get money back
 
-        Given   The employee is in request field on homepage
+        # Given   The employee is in request field on homepage
         When    The employee types request reason into request reason input
         When    The employee types dollar amount into dollar request input
         When    Then employee clicks submit request button
@@ -16,6 +16,6 @@ Feature: Employee should be able to view previous reimbursement requests
 
     Scenario: As an employee I want to log out
 
-        Given   The employee is on the manager homepage
+        # Given   The employee is on the manager homepage
         When    The employee clicks log out button
         Then    The employee should be logged out  
